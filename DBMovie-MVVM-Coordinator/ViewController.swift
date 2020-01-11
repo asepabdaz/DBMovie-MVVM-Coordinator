a309baca9f39f2013e717542dfc90553cda05a43
Asep Abdaz <asepabdaz@gmail.com>
2020-01-11 23:36:41 +0700

    Initial Commit

---
+++ DBMovie-MVVM-Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DBMovie-MVVM-Coordinator
+//
+//  Created by Asep Abdaz on 11/01/20.
+//  Copyright © 2020 Asep Abdaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
