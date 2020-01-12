d98d716be033a7b310c286b4d818102a989428db
Asep Zee <asep.abdulazis5@gmail.com>
2020-01-12 09:57:55 +0700

    Add
    MovieCoordinator.swift
    DetailMovieViewController.swift
    DetailMovieViewController.xib
    LoginViewController.swift
    LoginViewController.xib
    SignUpViewController.swift
    SignUpViewController.xib

---
+++ DBMovie-MVVM-Coordinator/Application/Coordinator/MovieCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  MovieCoordinator.swift
+//  DBMovie-MVVM-Coordinator
+//
+//  Created by Asep Abdaz on 12/01/20.
+//  Copyright © 2020 Asep Abdaz. All rights reserved.
+//
+
+import Foundation
